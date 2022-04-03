1a54af41633320a0c099da0ff1ed0f2349cf8a00
Joseph Long <github@joseph-long.com>
2022-04-03 15:13:12 -0400

    Ray grid support

--- xconf/contrib/__init__.py
+++ xconf/contrib/__init__.py
@@ -1,4 +1,7 @@
+import fsspec
 from typing import Optional, Union
+import typing
+import os.path
 from urllib.parse import urlparse
 import threading
 import logging
@@ -11,15 +14,17 @@ from .. import field, config, Command
 
 @config
 class CommonRayConfig:
+    setup_function_path : typing.ClassVar[typing.Optional[str]] = None
     env_vars : Optional[dict[str, str]] = field(default_factory=dict, help="Environment variables to set for worker processes")
 
     def _get_ray_init_kwargs(self):
         env_vars = {
-            'RAY_USER_SETUP_FUNCTION': 'xpipeline.commands.vapp_trap.init_worker',
             'MKL_NUM_THREADS': '1',
             'OMP_NUM_THREADS': '1',
             'NUMBA_NUM_THREADS': '1',
         }
+        if self.setup_function_path is not None:
+            env_vars['RAY_USER_SETUP_FUNCTION'] = self.setup_function_path
         env_vars.update(self.env_vars)
         ray_init_kwargs = {'runtime_env': {'env_vars': env_vars,}}
         return ray_init_kwargs
@@ -51,6 +56,21 @@ class RemoteRayConfig(CommonRayConfig):
         ray_init_kwargs['address'] = self.url
         return ray_init_kwargs
 
+AnyRayConfig = Union[LocalRayConfig, RemoteRayConfig]
+
+def join(*args):
+    '''URL-aware path joining, working equally well on file paths and
+    URLs with schemes like https://'''
+    res = urlparse(args[0])
+    new_path = os.path.join(res.path, *args[1:])
+    return args[0].replace(res.path, new_path)
+
+def basename(path):
+    '''URL-aware path basename, working equally well on file paths and
+    URLs with schemes like https://'''
+    res = urlparse(path)
+    return os.path.basename(res.path)
+
 def get_fs(path):
     '''Obtain a concrete fsspec filesystem from a path using
     the protocol string (if any; defaults to 'file:///') and
@@ -61,7 +81,6 @@ def get_fs(path):
     Note: Won't work when kwargs are required but not encoded in the
     URL.
     '''
-    import fsspec
     scheme = urlparse(path).scheme
     proto = scheme if scheme != '' else 'file'
     cls = fsspec.get_filesystem_class(proto)
@@ -82,13 +101,22 @@ def get_fs(path):
 class PathConfig:
     path : str = field(help="File path")
 
-    def get_fs(self):
+    def get_fs(self) -> fsspec.AbstractFileSystem:
         return get_fs(self.path)
 
 @config
-class FileConfig(PathConfig):
+class DirectoryConfig(PathConfig):
+    def exists(self):
+        return self.get_fs().exists(self.path)
+    def ensure_exists(self):
+        destfs = self.get_fs()
+        destfs.makedirs(self.path, exist_ok=True)
+    def open_path(self, path, mode="rb"):
+        return self.get_fs().open(join(self.path, path), mode=mode)
 
-    def open(self, mode='r'):
+@config
+class FileConfig(PathConfig):
+    def open(self, mode='rb') -> fsspec.core.OpenFile:
         fs = get_fs(self.path)
         return fs.open(self.path, mode)
 
@@ -97,7 +125,7 @@ INDEX_COLNAME = 'index'
 
 @config
 class BaseRayGrid(Command):
-    destination : PathConfig = field(default=".", help="Directory for output files")
+    destination : PathConfig = field(default=PathConfig(path="."), help="Directory for output files")
     output_filename : str = field(default="grid.fits", help="Output filename to write")
     output_extname : str = field(default="grid", help="Output table extension name")
     ray : Union[RemoteRayConfig, LocalRayConfig] = field(
@@ -109,24 +137,35 @@ class BaseRayGrid(Command):
     recompute : bool = field(default=False, help="Whether to ignore existing checkpoints and recompute everything")
     shutdown_on_completion : bool = field(default=True, help="Whether ray.shutdown() is called to clean up the cluster after completing the grid")
     hide_progress_bar : bool = field(default=None, help="Whether to hide the progress bar, default chooses to hide only when stdout is not a tty")
-    checkpoint_every_x : int = field(default=10, help="How often to write a grid checkpoint")
+    checkpoint_every_x : int = field(default=10, help="How many results to collect before writing a grid checkpoint (approximately), set 0 to disable")
 
     def main(self):
+        self.ray.init()
         tbl = self.load_checkpoint_or_generate()
+        if TIME_COLNAME not in tbl.dtype.fields:
+            raise RuntimeError(f"Grid table doesn't include {TIME_COLNAME} column")
+        if INDEX_COLNAME not in tbl.dtype.fields:
+            raise RuntimeError(f"Grid table doesn't include {INDEX_COLNAME} column")
         pending_tbl = self.filter_grid(tbl)
+        log.debug(f"{len(pending_tbl)} points to process")
         refs = self.launch_grid(pending_tbl)
+        log.debug(f"Got {len(refs)} pending refs")
         self.process_grid(tbl, refs)
 
-    def load_checkpoint_or_generate(self):
+    def _get_output_path(self):
+        return join(self.destination.path, self.output_filename)
+
+    def load_checkpoint_or_generate(self, *args):
         from astropy.io import fits
-        empty_grid_tbl = self.generate_grid()
-        output_path = f"{self.destination}/{self.output_filename}"
+        import numpy as np
+        empty_grid_tbl = self.generate_grid(*args)
+        output_path = self._get_output_path()
         destfs = self.destination.get_fs()
         if not destfs.exists(output_path):
             log.debug(f"No checkpoint at {output_path}, using generated grid")
             return empty_grid_tbl
-        with destfs.open(self.output_filename) as fh:
-            tbl = fits.open(fh)[self.output_extname].data
+        with destfs.open(output_path) as fh:
+            tbl = np.asarray(fits.open(fh)[self.output_extname].data).byteswap().newbyteorder()
         log.debug(f"Loaded checkpoint from {output_path}[{self.output_extname}]")
         if not self.compare_grid_to_checkpoint(tbl, empty_grid_tbl):
             raise RuntimeError(f"Grid parameters changed but checkpoint file exists, aborting")
@@ -134,24 +173,28 @@ class BaseRayGrid(Command):
 
     def save(self, tbl):
         from astropy.io import fits
-        with self.destination.get_fs().open(self.output_filename, 'wb') as fh:
+        output_path = self._get_output_path()
+        with self.destination.get_fs().open(output_path, 'wb') as fh:
             fits.HDUList([
                 fits.PrimaryHDU(),
                 fits.BinTableHDU(tbl, name=self.output_extname),
             ]).writeto(fh, overwrite=True)
-        log.debug(f"Saved to {self.destination}/{self.output_filename}")
+        log.debug(f"Saved to {output_path}")
         return tbl
 
     def filter_grid(self, tbl):
         import numpy as np
+        mask = np.ones(len(tbl), dtype=bool)
         if len(self.only_indices):
-            mask = np.zeros(len(tbl), dtype=bool)
             for idx in self.only_indices:
                 mask |= tbl[INDEX_COLNAME] == idx
         mask &= tbl[TIME_COLNAME] == 0
         return tbl[mask]
 
     def process_grid(self, tbl, pending_refs):
+        if len(pending_refs) == 0:
+            log.info("All requested grid points already computed")
+            return
         import ray
         import numpy as np
         from tqdm import tqdm
@@ -163,13 +206,13 @@ class BaseRayGrid(Command):
         with tqdm(total=total, disable=self.hide_progress_bar, unit='points') as pbar:
             pbar.update(restored_from_checkpoint)
             while pending:
-                complete, pending = ray.wait(pending, timeout=5, num_returns=min(self.checkpoint_every_x, len(pending)))
+                complete, pending = ray.wait(pending, timeout=5, num_returns=min(self.checkpoint_every_x if self.checkpoint_every_x > 0 else 1, len(pending)))
                 for result in ray.get(complete):
                     idx = result[INDEX_COLNAME]
                     tbl[idx] = result
                 if len(complete):
                     pbar.update(len(complete))
-                    if self.checkpoint is not None:
+                    if self.checkpoint_every_x > 0:
                         self.save(tbl)
         self.save(tbl)
 
